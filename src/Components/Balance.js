bd62362542e192cc24d549d4592eda81bc4c95ff
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-16 14:02:17 +0200

    Balance Component Started

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import BalanceLogo from '../Images/balance-img.png'
 
 export default function Balance() {
   return (
     <div className='balance-container'>
         <div className='my-balance'>
-            <p></p>
-
+            <p className='my-balance-p1'>My balance</p>
+            <p className='my-balance-p2'>$921.48</p>
         </div>
+        <img src={BalanceLogo} width='60' height='40'></img>
     </div>
   )
 }
