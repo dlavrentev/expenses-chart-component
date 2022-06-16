263fb110cd5b3796d6d3331d0f44b91fdca32e3d
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-16 15:11:21 +0200

    Update Spending.js

--- src/Components/Spending.js
+++ src/Components/Spending.js
@@ -4,6 +4,56 @@ export default function Spending() {
   return (
     <div className='spending-container'>
         
+        <p>Spending - Last 7 Days</p>
+        
+        <div className='chart-container'>
+          
+          <div className='day-container'>
+            <div className='chart-container'></div>
+            <p>mon</p>
+          </div>
+          
+          <div className='day-container'>
+            <div className='chart-container'></div>
+            <p>tue</p>
+          </div>
+          
+          <div className='day-container'>
+            <div className='chart-container'></div>
+            <p>wed</p>
+          </div>
+          
+          <div className='day-container'>
+            <div className='chart-container'></div>
+            <p>thu</p>
+          </div>
+          
+          <div className='day-container'>
+            <div className='chart-container'></div>
+            <p>fri</p>
+          </div>
+          
+          <div className='day-container'>
+            <div className='chart-container'></div>
+            <p>sat</p>
+          </div>
+          
+          <div className='day-container'>
+            <div className='chart-container'></div>
+            <p>sun</p>
+          </div>
+        </div>
+        
+        <div>
+            <div>
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
 
     </div>
   )
