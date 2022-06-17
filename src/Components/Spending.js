ecd7d40dc93fea738ca423afcf90edb1201e39d3
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-17 09:18:35 +0200

    css update

--- src/Components/Spending.js
+++ src/Components/Spending.js
@@ -9,49 +9,49 @@ export default function Spending() {
         <div className='chart-container'>
           
           <div className='day-container'>
-            <div className='chart-container'></div>
+            <div className='money-container'>1</div>
             <p>mon</p>
           </div>
           
           <div className='day-container'>
-            <div className='chart-container'></div>
+            <div className='money-container'>1</div>
             <p>tue</p>
           </div>
           
           <div className='day-container'>
-            <div className='chart-container'></div>
+            <div className='money-container'>1</div>
             <p>wed</p>
           </div>
           
           <div className='day-container'>
-            <div className='chart-container'></div>
+            <div className='money-container'>1</div>
             <p>thu</p>
           </div>
           
           <div className='day-container'>
-            <div className='chart-container'></div>
+            <div className='money-container'>1</div>
             <p>fri</p>
           </div>
           
           <div className='day-container'>
-            <div className='chart-container'></div>
+            <div className='money-container'>1</div>
             <p>sat</p>
           </div>
           
           <div className='day-container'>
-            <div className='chart-container'></div>
+            <div className='money-container'>1</div>
             <p>sun</p>
           </div>
         </div>
         
         <div className='month-container'>
             <div className='month-container-left'>
-                <p className='month-p1'></p>
-                <p className='month-p2'></p>
+                <p className='month-p1'>Total this month</p>
+                <p className='month-p2'>$478.33</p>
             </div>
             <div className='month-container-right'>
-                <p className='month-p3'></p>
-                <p className='month-p4'></p>
+                <p className='month-p3'>+2.4%</p>
+                <p className='month-p4'>from last month</p>
             </div>
         </div>
 
